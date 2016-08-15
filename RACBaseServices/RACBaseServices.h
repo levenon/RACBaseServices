72a267c2b2a8c00072f7975c6fd74944fe478012
MarkeJave <308865427@qq.com>
2016-08-15 16:14:24 +0800

    no message

--- RACBaseServices/RACBaseServices.h
+++ RACBaseServices/RACBaseServices.h
@@ -16,4 +16,23 @@ FOUNDATION_EXPORT const unsigned char RACBaseServicesVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RACBaseServices/PublicHeader.h>
 
-
+#import "RACControllerViewModel.h"
+#import "RACCollectionViewModel.h"
+#import "RACReactiveViewModel.h"
+#import "RACSegmentedControlViewModel.h"
+#import "RACTableViewModel.h"
+#import "RACViewModel.h"
+#import "RACWebViewModel.h"
+#import "RACDoubleTitleView.h"
+#import "RACLoadingTitleView.h"
+#import "RACViewControllerAnimatedTransition.h"
+#import "RACWebViewController.h"
+#import "RACReactiveView.h"
+#import "RACViewController.h"
+#import "RACAppStoreService.h"
+#import "RACControllerProtocol.h"
+#import "RACNavigationControllerStack.h"
+#import "RACNavigationProtocol.h"
+#import "RACRouter.h"
+#import "RACViewModelServices.h"
+#import "RACViewModelServicesImpl.h"
